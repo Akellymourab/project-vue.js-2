d9e2f7d7c7694decbb04939b0342933df6253875
Ana Kelly <anakellymourab@gmail.com>
2023-10-24 01:07:04 -0300

    starting project with a CepTable component

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import App from './App.vue'
+import CepTableVue from './components/CepTable.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(ElementPlus, { size: 'small', zIndex: 3000 })
+app.component('CepTable', CepTableVue)
+app.mount('#app') 
